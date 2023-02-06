e5f19ad3d0efd83207face84bdee7b6ce3aa1b1a
Frah01 <vinario.francesco@gmail.com>
2023-02-06 22:26:36 +0100

    Aggiunto style e messe icone fontawesome

--- database.php
+++ database.php
@@ -6,10 +6,10 @@ include __DIR__ . './Models/Cibo.php';
 
 // CREO ISTANZE PER LA SEZIONE CIBO
 
-$cibo1 = new Cibo('./img/royal-canin.jpg', 'Royal Canin Mini Adult', 'Cane', 'Prezzo:€ 43,99', 'Peso netto: 545g', 'Ingredienti: prosciutto, riso');
-$cibo2 = new Cibo('./img/almo-nature.jpg', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane', 'Prezzo: €44,99', 'Peso netto: 600g', 'Ingredienti: manzo, cereali');
-$cibo3 = new Cibo('./img/almo-nature-cat.jpg', 'Almo Nature Cat Daily Lattina', 'Gatto', 'Prezzo: €34,99', 'Peso netto: 400g', 'Ingredienti: tonno, pollo, prosciutoo');
-$cibo4 = new Cibo('./img/guppy.jpg', 'Mangime per Pesci Guppy in Fiocchi', 'Pesce', 'Prezzo: €2,95', 'Peso netto: 30g', 'Ingredienti: Pesi e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
+$cibo1 = new Cibo('./img/royal-canin.jpg', 'Royal Canin Mini Adult', '<i class="fa-solid fa-dog"></i> Cane', 'Prezzo:€ 43,99', 'Peso netto: 545g', 'Ingredienti: prosciutto, riso');
+$cibo2 = new Cibo('./img/almo-nature.jpg', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', '<i class="fa-solid fa-dog"></i> Cane', 'Prezzo: €44,99', 'Peso netto: 600g', 'Ingredienti: manzo, cereali');
+$cibo3 = new Cibo('./img/almo-nature-cat.jpg', 'Almo Nature Cat Daily Lattina', '<i class="fa-solid fa-cat"></i> Gatto', 'Prezzo: €34,99', 'Peso netto: 400g', 'Ingredienti: tonno, pollo, prosciutoo');
+$cibo4 = new Cibo('./img/guppy.jpg', 'Mangime per Pesci Guppy in Fiocchi', '<i class="fa-solid fa-fish-fins"></i> Pesce', 'Prezzo: €2,95', 'Peso netto: 30g', 'Ingredienti: Pesi e sottoprodotti dei pesci, Cereali, Lieviti, Alghe');
 
 $cibi_array = [
     $cibo1,
@@ -20,8 +20,8 @@ $cibi_array = [
 
 // CREO ISTANZE PER LA SEZIONE ACCESSORI
 
-$accessorio1 = new Accessori('./img/voliera-wilma.webp', 'Voliera Wilma in Legno', 'Uccello', 'Prezzo: €184,99', 'Materiale: Legno', 'Dimensioni: M: L 83 xx P67 x H153 cm');
-$accessorio2 = new Accessori('./img/cartucce.webp', 'Cartucce Filtranti per Filtro EasyCrystal', 'Pesce', 'Prezzo: €2,29', 'Materiale: Materiale espanso', 'Dimensioni: ND');
+$accessorio1 = new Accessori('./img/voliera-wilma.webp', 'Voliera Wilma in Legno', '<i class="fa-solid fa-crow"></i> Uccello', 'Prezzo: €184,99', 'Materiale: Legno', 'Dimensioni: M: L 83 xx P67 x H153 cm');
+$accessorio2 = new Accessori('./img/cartucce.webp', 'Cartucce Filtranti per Filtro EasyCrystal', '<i class="fa-solid fa-fish-fins"></i> Pesce', 'Prezzo: €2,29', 'Materiale: Materiale espanso', 'Dimensioni: ND');
 
 $accessori_array = [
     $accessorio1,
@@ -30,8 +30,8 @@ $accessori_array = [
 
 // CREO ISTANZE PER LA SEZIONE GIOCATTOLI
 
-$giocattolo1 = new Giocattoli('./img/kong.jpg', 'Kong Classic', 'Cane', 'Prezzo: €13,49', 'Caratteristiche: Galleggia e rimbalza', 'Dimensioni: M: L 83 xx P67 x H153 cm');
-$giocattolo2 = new Giocattoli('./img/topo.jpg', 'Topini di peluche Trixie', 'Gatto', 'Prezzo: €4,99', 'Caratteristiche: Morbido con codina in corda', 'Dimensioni: 8,5cm x 10 cm');
+$giocattolo1 = new Giocattoli('./img/kong.jpg', 'Kong Classic', '<i class="fa-solid fa-dog"></i> Cane', 'Prezzo: €13,49', 'Caratteristiche: Galleggia e rimbalza', 'Dimensioni: M: L 83 xx P67 x H153 cm');
+$giocattolo2 = new Giocattoli('./img/topo.jpg', 'Topini di peluche Trixie', '<i class="fa-solid fa-cat"></i> Gatto', 'Prezzo: €4,99', 'Caratteristiche: Morbido con codina in corda', 'Dimensioni: 8,5cm x 10 cm');
 
 $giocattoli_array = [
     $giocattolo1,
--- index.php
+++ index.php
@@ -13,18 +13,25 @@ include __DIR__ . '/database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 
 <body>
     <div class="container">
         <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Shop Online</h1>
+            </div>
+        </div>
+        <div class="row">
 
             <!-- CREO IL CICLO PER LA SEZIONE CIBO -->
 
             <?php foreach ($cibi_array as $cibi) { ?>
                 <div class="col-4 my-2">
-                    <div class="card">
+                    <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
                         <img src="<?php echo $cibi->image ?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $cibi->name ?></h5>
